c005ee8bd5a3c71080256e053d263a1dd1995155
emalhaolautaro <emalhao.142047@alu.uns.edu.ar>
2026-08-20 19:28:47 -0300

    Mejora del hud, barra de vida del nivel

--- levels/level.gd
+++ levels/level.gd
@@ -1,10 +1,11 @@
 extends Node2D
-
 @onready var player: CharacterBody2D = $Player
 @onready var hud: CanvasLayer = $HUD
 
+@export var health_per_battery := 25
 
 var batteries_remaining := 0
+var total_batteries := 0
 var game_finished := false
 
 
@@ -14,8 +15,8 @@ func _ready() -> void:
 			enemy.player = player
 
 	var batteries := get_tree().get_nodes_in_group("batteries")
-	batteries_remaining = batteries.size()
-
+	total_batteries = batteries.size()
+	batteries_remaining = total_batteries
 	print("Baterías encontradas: ", batteries_remaining)
 
 	for battery in batteries:
@@ -23,25 +24,29 @@ func _ready() -> void:
 		battery.destroyed.connect(_on_battery_destroyed)
 
 	player.died.connect(_on_player_died)
-	
+
 	if hud:
 		player.max_health_changed.connect(hud.update_max_health)
 		player.health_changed.connect(hud.update_health)
 		hud.update_max_health(player.max_health)
 		hud.update_health(player.health)
-		
+
 		player.max_energy_changed.connect(hud.update_max_energy)
 		player.energy_changed.connect(hud.update_energy)
 		hud.update_max_energy(player.max_energy)
 		hud.update_energy(player.energy)
+		hud.update_max_supercomputer_health(total_batteries * health_per_battery)
+		hud.update_supercomputer_health(batteries_remaining * health_per_battery)
 
 
 func _on_battery_destroyed() -> void:
 	if game_finished:
 		return
-
 	batteries_remaining -= 1
 	print("Batería destruida. Quedan: ", batteries_remaining)
+
+	if hud:
+		hud.update_supercomputer_health(batteries_remaining * health_per_battery)
 
 	if batteries_remaining <= 0:
 		finish_level(true)
@@ -50,7 +55,6 @@ func _on_battery_destroyed() -> void:
 func _on_player_died() -> void:
 	if game_finished:
 		return
-
 	finish_level(false)
 
 
--- ui/hud/hud.gd
+++ ui/hud/hud.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
-
 @onready var health_bar: TextureProgressBar = $HealthBar
 @onready var energy_bar: TextureProgressBar = $EnergyBar
+@onready var supercomputer_health_bar: TextureProgressBar = $SupercomputerHealthBar
 
 func update_max_health(max_health: int) -> void:
 	health_bar.max_value = max_health
@@ -15,3 +15,9 @@ func update_max_energy(max_energy: float) -> void:
 
 func update_energy(new_energy: float) -> void:
 	energy_bar.value = new_energy
+
+func update_max_supercomputer_health(max_health: int) -> void:
+	supercomputer_health_bar.max_value = max_health
+
+func update_supercomputer_health(new_health: int) -> void:
+	supercomputer_health_bar.value = new_health
